9a97d13dd3b27fd96ef7be6961ad89b9dc3b085d
NicFisker <NicFisker@ts-mbp-2.home>
2020-11-15 12:11:23 +0100

    scss tilføjet til componenter

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,20 @@
-import { Component } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
-@Component({ templateUrl: 'home.component.html' })
-export class HomeComponent {}
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+  })
+
+export class NavbarComponent implements OnInit {
+    constructor(private router: Router, public auth: AuthenticationService) {}
+  
+    ngOnInit(): void {}
+  
+    logout() {
+      this.auth.logout();
+      this.router.navigate(['/']);
+    }
+  }
